92c7d0ec13fd2b219ec8b246a57bc840bbea6e1d
Aditya Viren <adityaviren@gmail.com>
2020-10-25 12:18:20 +0530

    ADDED test for exception type

--- src/test/java/com/cg/stateanalyser/StateAnalyserTest.java
+++ src/test/java/com/cg/stateanalyser/StateAnalyserTest.java
@@ -8,15 +8,15 @@ import java.io.IOException;
 
 public class StateAnalyserTest {
 
-    @Test
+    /*@Test
     public void givenStateAnalyser_shouldReturnCount() throws IOException, CsvValidationException {
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
         long noOfEntries = stateCensusAnalyser.loadStateCensus().stream().count();
         Assert.assertEquals(28,noOfEntries);
 
-    }
+    }*/
 
-    /*@Test
+    @Test
     public void givenStateAnalyser_shouldReturnException() throws CsvValidationException, IOException, StateCensusException {
         try {
             StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
@@ -24,5 +24,5 @@ public class StateAnalyserTest {
         }catch (StateCensusException e) {
             Assert.assertEquals(StateCensusException.Exception_type.Census_file_problem, e.type);
         }
-    }*/
+    }
 }
